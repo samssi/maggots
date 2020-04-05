a9175e0e2597cb7e1d3ef053e533986f5d14c20e
samssi <samir.hamadi@gmail.com>
2020-04-05 20:32:48 +0300

    Add project

---
+++ app/src/main/java/fi/maggots/MainActivity.kt
@@ -0,0 +1,14 @@
+package fi.maggots
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PowerManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
