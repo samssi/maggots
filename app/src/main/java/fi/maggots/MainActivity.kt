d4779473cf57144ecb74c841df54399f9562f35c
samssi <samir.hamadi@gmail.com>
2020-04-17 10:28:07 +0300

    Working hello world based on Android SDK instructions

--- app/src/main/java/fi/maggots/MainActivity.kt
+++ app/src/main/java/fi/maggots/MainActivity.kt
@@ -1,17 +1,137 @@
 package fi.maggots
 
+import android.content.Context
+import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import fi.maggots.renderer.GameRenderer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var glView: GLSurfaceView
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        val view = GLSurfaceView(this)
-        view.setRenderer(GameRenderer())
-        setContentView(view)
+        glView = MaggotsSurfaceView(this)
+        setContentView(glView)
+    }
+}
+
+class MaggotsSurfaceView(context: Context) : GLSurfaceView(context) {
+    private val renderer: GameRenderer3
+
+    init {
+        setEGLContextClientVersion(2)
+        renderer = GameRenderer3()
+        setRenderer(renderer)
+        renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+    }
+}
+
+private lateinit var mTriangle: Triangle
+
+class GameRenderer3 : GLSurfaceView.Renderer {
+    override fun onDrawFrame(gl: GL10?) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        mTriangle.draw()
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0,0, width, height)
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        // Set background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        mTriangle = Triangle()
+    }
+}
+
+const val COORDS_PER_VERTEX = 3
+var triangleCoords = floatArrayOf(     // in counterclockwise order:
+    0.0f, 0.5f, 0.0f,      // top
+    -0.5f, -0.5f, 0.0f,    // bottom left
+    0.5f, -0.5f, 0.0f      // bottom right
+)
+
+class Triangle {
+    private val vertexShaderCode =
+        "attribute vec4 vPosition;" +
+                "void main() {" +
+                "  gl_Position = vPosition;" +
+                "}"
+
+    private val fragmentShaderCode =
+        "precision mediump float;" +
+                "uniform vec4 vColor;" +
+                "void main() {" +
+                "  gl_FragColor = vColor;" +
+                "}"
+
+    private fun loadShader(type: Int, shaderCode: String): Int {
+        return GLES20.glCreateShader(type).also {
+                shader ->
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+        }
+    }
+
+    private var mProgram: Int
+
+    init {
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        mProgram = GLES20.glCreateProgram().also {
+            GLES20.glAttachShader(it, vertexShader)
+            GLES20.glAttachShader(it, fragmentShader)
+            GLES20.glLinkProgram(it)
+        }
+    }
+
+    // Set color with red, green, blue and alpha (opacity) values
+    val color = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
+
+    private var vertexBuffer: FloatBuffer =
+        // (number of coordinate values * 4 bytes per float)
+        ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
+            // use the device hardware's native byte order
+            order(ByteOrder.nativeOrder())
+            asFloatBuffer().apply {
+                put(triangleCoords)
+                position(0)
+            }
+        }
+
+    private var positionHandle: Int = 0
+    private var mColorHandle: Int = 0
+    private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
+    private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
+
+    fun draw() {
+        GLES20.glUseProgram(mProgram)
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition").also {
+            GLES20.glEnableVertexAttribArray(it)
+
+            GLES20.glVertexAttribPointer(
+                it,
+                COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT,
+                false,
+                vertexStride,
+                vertexBuffer
+            )
+
+            mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also {
+                colorHandle -> GLES20.glUniform4fv(colorHandle, 1, color, 0)
+                GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
+                GLES20.glDisableVertexAttribArray(it)
+            }
+        }
     }
 }
