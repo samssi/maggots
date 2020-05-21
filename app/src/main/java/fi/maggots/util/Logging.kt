45e2c1fba7d760e99be67bc78c89ecf7e904a38f
samssi <samir.hamadi@gmail.com>
2020-05-21 18:45:33 +0300

    Add logging and single tap detection

---
+++ app/src/main/java/fi/maggots/util/Logging.kt
@@ -0,0 +1,3 @@
+package fi.maggots.util
+
+internal val DEBUG_TAG = "DEBUG"
--- app/src/main/java/fi/maggots/view/MaggotsSurfaceView.kt
+++ app/src/main/java/fi/maggots/view/MaggotsSurfaceView.kt
@@ -2,30 +2,52 @@ package fi.maggots.view
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
+import androidx.core.view.GestureDetectorCompat
 import fi.maggots.renderer.GameRenderer
+import fi.maggots.util.DEBUG_TAG
 
 internal const val TOUCH_SCALE_FACTOR: Float = 180.0f / 320f
 
-class MaggotsSurfaceView(context: Context) : GLSurfaceView(context) {
+class MaggotsSurfaceView(context: Context) : GLSurfaceView(context), GestureDetector.OnGestureListener {
     private val renderer: GameRenderer
+    private var mDetector: GestureDetectorCompat
 
     init {
         setEGLContextClientVersion(2)
         renderer = GameRenderer(context)
         setRenderer(renderer)
+        mDetector = GestureDetectorCompat(context, this)
 
         // Render the view only when there is a change in the drawing data
         renderMode = RENDERMODE_WHEN_DIRTY
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        when(event.action) {
-            MotionEvent.ACTION_MOVE -> {
-                renderer.mTriangle.angle += renderer.mTriangle.move(event.x, event.y, height, width)
-                requestRender()
+        return if (mDetector.onTouchEvent(event)) {
+            true
+        } else {
+            when(event.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    renderer.mTriangle.angle += renderer.mTriangle.move(event.x, event.y, height, width)
+                    requestRender()
+                }
             }
+            return true
         }
+
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        Log.d(DEBUG_TAG, "singleTapUp")
         return true
     }
+
+    override fun onShowPress(e: MotionEvent?) { }
+    override fun onDown(event: MotionEvent?): Boolean { return false }
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean { return false }
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean { return false }
+    override fun onLongPress(e: MotionEvent?) { }
 }
